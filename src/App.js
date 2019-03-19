034156cebd12c76f9014fdf58c1a9b7cb7b932f2
Christina Ajemyan <qristine@brainfors.com>
2019-03-19 15:48:16 +0400

    no tabs version

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React, {Component} from 'react';
 import './App.css';
 import Header from './components/header/header.component'
+import Home from './components/home/home.component'
 
 class App extends Component {
+  constructor(prop){
+    super(prop);
+    this.child = React.createRef();
+  }
+  handleClick(view){
+    this.child.current.changeView(view)
+  }
   render() {
     return (
       <div className="App">
         <div className="container-fluid">
-          <Header/>
+          <Header handleClick={this.handleClick.bind(this)}/>
+          <Home ref={this.child}/>
         </div>
       </div>
     );
--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -1,71 +1,18 @@
 import React, { Component } from 'react';
-import {Tabs, Tab} from 'react-bootstrap'
-import Users from '../users/user.omponent'
-import Points from '../points/points.components'
-import UserDetails from '../userDetails/userDetails.component'
-import PointDetails from '../pointDetails/pointDetails.component'
-
+import {Navbar, Nav} from 'react-bootstrap'
 
 class Header extends Component {
     constructor(props){
         super(props);
-        this.state={
-            user: {},
-            point: {}
-        }
-        this.handleSelect = (item, type)=>{
-            setTimeout(()=>{
-              type==='user'? this.setState({user: item}):this.setState({point:item}) 
-            },100)
-        }
-        this.childUser = React.createRef();
-        this.childPoint = React.createRef();
-    }
-    saveUserInfo(user, e){
-        e.preventDefault()
-        this.childUser.current.changeUserInfo(user)
-    }
-    savePointInfo(user, e){
-        e.preventDefault()
-        this.childPoint.current.changePointInfo(user)
-    }
-    deleteUser(userId, e){
-        e.preventDefault()
-        this.childUser.current.deleteUser(userId)
-    }
-    deletePoint(id, e){
-        e.preventDefault()
-        this.childPoint.current.deletePoint(id)
+        this.handleClick=props.handleClick
     }
     
     render() {
         return (
-            <Tabs defaultActiveKey="users" id="uncontrolled-tab-example">
-                <Tab eventKey="users" title="Users">
-                    <div className="main">
-                        <div className="row">
-                            <div className="col-md-3">
-                                <Users ref={this.childUser} handleSelect={this.handleSelect}/>
-                            </div>
-                            <div className="col-md-9">
-                                <UserDetails user={this.state.user} saveUserInfo={this.saveUserInfo.bind(this)} deleteUser={this.deleteUser.bind(this)}/>
-                            </div>
-                        </div>
-                    </div>
-                </Tab>
-                <Tab eventKey="points" title="Points">
-                    <div className="main">
-                        <div className="row">
-                            <div className="col-md-4">
-                                <Points ref={this.childPoint} handleSelect={this.handleSelect}/>
-                            </div>
-                            <div className="col-md-8">
-                                <PointDetails point={this.state.point} savePointInfo={this.savePointInfo.bind(this)} deletePoint={this.deletePoint.bind(this)}/>
-                            </div>
-                        </div>
-                    </div>
-                </Tab>
-            </Tabs>
+            <Navbar bg="light" expand="lg">
+                <Nav.Link onClick={this.handleClick.bind(this, 'users')}>Users</Nav.Link>
+                <Nav.Link  onClick={this.handleClick.bind(this, 'points')}>Points</Nav.Link>
+            </Navbar>
         );
     }
 }
